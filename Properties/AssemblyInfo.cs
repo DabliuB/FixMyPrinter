6468742be957f6fc862f25bc89dcad004d2bd5c9
WILLIAM Silva <wsilva@pontiffex.com.br>
2022-02-07 06:52:58 -0300

    Alteração da versão no arquivo AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Corrigir problemas comuns associados à fila de impressão")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pontiffex Sistemas")]
-[assembly: AssemblyProduct("Pintiffex FixMyPrinter")]
+[assembly: AssemblyProduct("Pontiffex FixMyPrinter")]
 [assembly: AssemblyCopyright("Copyright © 2022, Pontiffex Sistemas. Direitos reservados.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.06.02.22")]
 [assembly: AssemblyFileVersion("1.06.02.22")]
